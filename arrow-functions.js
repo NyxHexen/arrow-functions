4de39b631d6e7de08164b26a2b1a5cade0fd028a
NyxHexen <jd.davis948@gmail.com>
2022-07-04 19:44:23 +0100

    Module complete

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,32 @@
-/**
- * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
- */
-
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b){
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum1 = addTwoNumbers(3, 5);
+console.log(sum1);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+
+//If no parameter, use empty set of parentheses to tell JS we're creating a function.
+const sayHello = () => console.log('hello'); 
 
 // Returning Multiple Lines
+// Uses () instead of {};
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiple lines string!
+    </p>`
+);
